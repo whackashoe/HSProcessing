1ac4066337611738a6af9891ec7228b504c988bc
Jeffrey Drake <drakej@uwindsor.ca>
2012-08-09 01:23:54 -0400

    Updated Shaders, just need to add error checking, then can test directly

--- src/Processing/Shader.hs
+++ src/Processing/Shader.hs
@@ -2,6 +2,7 @@
 module Processing.Shader where
 
 import Graphics.Rendering.OpenGL.Raw.Core32
+import Control.Applicative
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 import qualified Data.ByteString.Unsafe as BSU
@@ -13,25 +14,26 @@ import Unsafe.Coerce
 
 newtype Shader = Shader GLuint 
     deriving (Eq, Show)
-{-
+
 
 makeShader :: T.Text -> T.Text -> IO Shader
-makeShader vertex fragment = do
+makeShader vertexsrc fragmentsrc = do
     vertex <- glCreateShader gl_VERTEX_SHADER
-    fragment <- glCreate
-    where compileShader typ text = do
-        shader <- gl_CreateShader typ
-        glShaderSource shader 1 
+    fragment <- glCreateShader gl_FRAGMENT_SHADER
+    shaderSource vertex vertexsrc
+    shaderSource fragment fragmentsrc
+    program <- glCreateProgram 
+    glAttachShader program vertex
+    glAttachShader program fragment
+    glLinkProgram program
+    pure $ Shader program
         
-export_to_C :: T.Text -> IO (Ptr CString)
-export_to_C input = do
-    -}
-runAddSource :: Shader -> T.Text -> IO ()
-runAddSource (Shader sh) text = BSU.unsafeUseAsCString bytestring shader
+shaderSource :: GLuint -> T.Text -> IO ()
+shaderSource sh text = BSU.unsafeUseAsCString bytestring shader
     where
         bytestring = TE.encodeUtf8 text
         shader cstr = 
-            withArray [cstr] (\s -> glShaderSource 1 1 ((castPtr s)::(Ptr (Ptr GLchar))) nullPtr)
+            withArray [cstr] (\s -> glShaderSource sh 1 ((castPtr s)::(Ptr (Ptr GLchar))) nullPtr)
 
 
 
