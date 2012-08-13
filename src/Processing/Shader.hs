fe2ff8f48d6af345517e0d3ae0b23ab58e9b0392
Jeffrey Drake <drakej@uwindsor.ca>
2012-08-13 03:44:56 -0400

    Abstracted more of the shader stuff, and can test some of the functions now

--- src/Processing/Shader.hs
+++ src/Processing/Shader.hs
@@ -1,48 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
 
 module Processing.Shader where
 
 import Graphics.Rendering.OpenGL.Raw.Core32
+import Graphics.Rendering.OpenGL.Raw.Core31.TypesInternal
 import Control.Applicative
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 import qualified Data.ByteString.Unsafe as BSU
 import Foreign.Marshal.Array
+import Foreign.Marshal.Alloc
 import Foreign.Ptr
 import Foreign.C.String
 import Foreign.C.Types
 import Unsafe.Coerce
-import qualified Data.Map as Map
+import Control.Error
+import Control.Monad.IO.Class
+import Foreign.Storable
+import Data.Bits
+import Data.Int
+import Data.Word
 
-newtype Shader = Shader GLuint 
+import Processing.Error
+
+newtype ShaderProgram = Shader GLuint 
     deriving (Eq, Show)
 
-    -- merci DMcGill_
-checkError :: IO [GLenum]
-checkError = do
-  e <- glGetError
-  if e == gl_NO_ERROR
-    then return []
-    else (e:) <$> checkError  
-      
+            
+tryGL :: IO r -> EitherT [Error] IO r
+tryGL m = liftIO $ m <* checkError
 
-errorStrings e 
-    | e == gl_NO_ERROR                          = "No error"
-    | e == gl_INVALID_ENUM                      = "Invalid enum"
-    | e == gl_INVALID_VALUE                     = "Invalid value"
-    | e == gl_INVALID_OPERATION                 = "Invalid Operation"
-    | e == gl_INVALID_FRAMEBUFFER_OPERATION     = "Invalid Framebuffer Operation"
-    | e == gl_OUT_OF_MEMORY                     = "Out of Memory. Fatal."
-    | otherwise                                 = "Unknown error."
+--formattedErrorCodes :: [GLenum] -> String
+--formattedErrorCodes = unlines . map errorStrings
+
+--reportErrors :: [GLenum] -> IO ()
+--reportErrors = putStrLn . formattedErrorCodes
 
 
-formattedErrorCodes :: [GLenum] -> String
-formattedErrorCodes = unlines . map errorStrings
-
-reportErrors :: [GLenum] -> IO ()
-reportErrors = putStrLn . formattedErrorCodes
-
-
-
+{-
 makeShader :: T.Text -> T.Text -> IO Shader
 makeShader vertexsrc fragmentsrc = do
     vertex <- glCreateShader gl_VERTEX_SHADER
@@ -55,43 +50,69 @@ makeShader vertexsrc fragmentsrc = do
     glLinkProgram program
     pure $ Shader program
         
-        
-shaderSource :: GLuint -> T.Text -> IO ()
+  -}      
+ 
+ {- 
+makeShader :: T.Text -> T.Text -> EitherT [Error] (IO Shader)
+makeShader vsrc fsrc = do
+    (vertex, fragment) <- createShaders
+    shaderSource vertex vsrc
+    shaderSource fragment fsrc
+    
+-}
+
+
+ -- from tekmo        
+createShaders :: EitherT [Error] IO (GLuint, GLuint)
+createShaders = do
+    vertex <- tryGL $ glCreateShader gl_VERTEX_SHADER
+    fragment <- tryGL $ glCreateShader gl_FRAGMENT_SHADER
+    pure (vertex, fragment)
+
+    
+shaderSource :: GLuint -> T.Text -> EitherT [Error] IO ()
 shaderSource sh text = do
-    BSU.unsafeUseAsCString bytestring shader
-    e <- checkError
-    case e of
-        [] -> pure ()
-        _ -> fail $ formattedErrorCodes e
+    tryGL $ BSU.unsafeUseAsCString bytestring shader
     where
         bytestring = TE.encodeUtf8 text
         shader cstr = 
             withArray [cstr] (\s -> glShaderSource sh 1 ((castPtr s)::(Ptr (Ptr GLchar))) nullPtr)
 
--- mm_freak suggested:
-{-
+compileShader :: GLuint -> EitherT [Error] IO T.Text
+compileShader shader = do
+    tryGL $ glCompileShader shader
+    s <- tryGL $ compileStatus shader
+    if s then tryGL $ infoLog shader
+     else pure "" 
 
-checkError :: EitherT Error IO ()
-
-
-createShaders :: EitherT Error IO (Shader, Shader)
-createShaders = do
-    vertex <- liftIO (glCreateShader gl_VERTEX_SHADER) <* checkError
-    fragment <- liftIO (glCreateShader gl_FRAGMENT_SHADER) <* checkError
-    pure (Shader vertex, Shader fragment)
+compileStatus :: GLuint -> IO Bool
+compileStatus shader = do
+    s <- alloca (\p -> glGetShaderiv shader gl_COMPILE_STATUS p >> peek p)
+    pure $ (glTruth . glIntToEnum) s
     
-    -}
+infoLog :: GLuint -> IO T.Text
+infoLog shader = do
+    (GLint logsize) <- infoLogLength shader
+    log' <- allocaArray (fromIntegral logsize) 
+            (\p -> glGetShaderInfoLog shader (GLsizei logsize) nullPtr p >> peekCString (unsafeCoerce p))
+    pure $ T.pack log'
     
-    -- or maybe something better with the free monad
-createShaders :: IO (Shader, Shader)
-createShaders = do
-    vertex <- glCreateShader gl_VERTEX_SHADER
-    e <- checkError
-    case e of
-        [] -> do
-            fragment <- glCreateShader gl_FRAGMENT_SHADER
-            e <- checkError 
-            case e of
-                [] -> pure (Shader vertex, Shader fragment)
-                _  -> fail $ formattedErrorCodes e
-        _  -> fail $ formattedErrorCodes e
+infoLogLength :: (Integral a) => GLuint -> IO a
+infoLogLength shader = do
+    (GLint s) <- alloca (\p -> glGetShaderiv shader gl_INFO_LOG_LENGTH p >> peek p)
+    pure $ (fromIntegral s)
+    
+
+
+glTruth :: GLenum -> Bool
+glTruth x = if x == gl_TRUE then True
+            else False
+            
+zzEncode32 :: Int32 -> Word32            
+zzEncode32 x = fromIntegral ((x `shiftL` 1) `xor` (x `shiftR` 31))
+
+glIntToEnum :: GLint -> GLenum
+glIntToEnum (GLint (CInt x)) = GLenum (CUInt (zzEncode32 x))
+
+-- fromGLint :: (Integral a, ) => GLint -> a
+--fromGLint (GLint x) = x
--- src/main.hs
+++ src/main.hs
@@ -8,6 +8,10 @@ import qualified Graphics.Rendering.OpenGL as GL
 import Graphics.Rendering.OpenGL.Raw.Core32 
 import qualified Graphics.UI.GLFW as GLFW
 
+import Control.Error
+
+import Processing.Shader
+
 main :: IO ()
 main = do
     _ <- GLFW.initialize
@@ -28,20 +32,12 @@ main = do
 
     glClearColor 0.05 0.05 0.05 1.0
     glDepthFunc gl_LESS
-  --  GL.clearColor    GL.$= GL.Color4 0.05 0.05 0.05 1
---    GL.depthFunc     GL.$= Just GL.Less
--- not in 3.2
---    GL.colorMaterial GL.$= Just (GL.FrontAndBack, GL.AmbientAndDiffuse)
---    GL.shadeModel    GL.$= GL.Smooth
+    
+    x <- runEitherT createShaders
+    case x of
+        Left e -> putStrLn $ unlines (map show e)
+        Right (x,y) -> putStrLn (show x ++ " " ++ show y)
 
-    
---    GL.lighting              GL.$= GL.Enabled
---    GL.lightModelAmbient     GL.$= GL.Color4 0.2 0.2 0.2 1
-  --  GL.position (GL.Light 0) GL.$= GL.Vertex4 (-10) 10 (-10) 0
---    GL.ambient  (GL.Light 0) GL.$= GL.Color4 0.4 0.4 0.4 1
---    GL.diffuse  (GL.Light 0) GL.$= GL.Color4 0.8 0.8 0.8 1
---    GL.light    (GL.Light 0) GL.$= GL.Enabled
-    
     loop
     GLFW.closeWindow
     GLFW.terminate
@@ -51,7 +47,7 @@ main = do
             GLFW.resetTime
             q0 <- GLFW.keyIsPressed GLFW.KeyEsc
             unless q0 $ do
-                glClear (gl_DEPTH 
+--                glClear (gl_DEPTH 
                 GL.clear [GL.ColorBuffer, GL.DepthBuffer]
                 GL.loadIdentity
                 GL.clearColor    GL.$= GL.Color4 1.0 0.05 0.05 1
